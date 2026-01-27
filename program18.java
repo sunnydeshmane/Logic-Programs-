4a98292a853dea2f484f2fb5d7b4db1b6c890053
SunnyDeshmane. <sunnydeshmane2004@gmail.com>
2026-01-27 17:40:08 +0530

    Add files via upload

---
+++ program18.java
@@ -0,0 +1,13 @@
+//worst apporach
+class program18
+{
+    public static void main(String a[])
+    {
+        //Sequence
+        
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+}
